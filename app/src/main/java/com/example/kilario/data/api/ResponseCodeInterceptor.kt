2b6049566b7d1282391f220918a3b6d74a72d5c8
Adnan <adnan9011@gmail.com>
2022-03-20 18:21:27 +0330

    Add and Config Retrofit With Hilt

---
+++ app/src/main/java/com/example/kilario/data/api/ResponseCodeInterceptor.kt
@@ -0,0 +1,32 @@
+package com.example.kilario.data.api
+
+import com.example.kilario.common.ErrorMessages
+import com.example.kilario.common.NetworkException
+import com.example.kilario.common.NotFoundException
+import com.google.gson.Gson
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.net.SocketTimeoutException
+import javax.inject.Inject
+
+class ResponseCodeInterceptor @Inject constructor(
+    val gson: Gson
+): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder().build()
+        val response = try {
+            chain.proceed(request)
+        } catch (exp : SocketTimeoutException) {
+            throw NetworkException(ErrorMessages.NETWORK_TIMEOUT.toString())
+        }
+
+        return when (response.code()) {
+
+            // @Todo Other responseCode like 401, 403
+            404 -> throw NotFoundException("Not Found")
+
+            else -> response
+        }
+    }
+}
