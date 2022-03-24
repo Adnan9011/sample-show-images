f3eff90cd9ddf15f04d404a064d114f87f5bc9e1
Adnan <adnan9011@gmail.com>
2022-03-24 20:10:16 +0430

    Add mapper to SharedMediaEntity.kt

--- app/src/main/java/com/example/kiliaro/data/db/sharedmedia/SharedMediaEntity.kt
+++ app/src/main/java/com/example/kiliaro/data/db/sharedmedia/SharedMediaEntity.kt
@@ -2,6 +2,8 @@ package com.example.kiliaro.data.db.sharedmedia
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.kiliaro.models.ui.SharedMediaUi
+import java.util.*
 
 const val SHARED_MEDIA_TABLE_NAME = "shared_media_table"
 const val SHARED_MEDIA_COLUMN_ID = "id"
@@ -13,9 +15,18 @@ const val SHARED_MEDIA_COLUMN_THUMBNAIL_URL = "thumbnailUrl"
 @Entity(tableName = SHARED_MEDIA_TABLE_NAME)
 data class SharedMediaEntity(
     @PrimaryKey
-    val id: Long,
+    val id: String,
     val sharedId: String,
     val size: Long,
     val createAt: Long,
     val thumbnailUrl: String
-)
+) {
+    fun toSharedMediaUi() = SharedMediaUi(
+        id = id,
+        size = size,
+        createAt = Calendar.getInstance().apply {
+            timeInMillis = createAt
+        },
+        thumbnailUrl = thumbnailUrl,
+    )
+}
