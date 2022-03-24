0688a50bcfeae5c88b9af2ff2bef7454de55ad36
Adnan <adnan9011@gmail.com>
2022-03-24 20:09:09 +0430

    Implement Dao for SharedMedia

--- app/src/main/java/com/example/kiliaro/data/db/sharedmedia/SharedMediaDao.kt
+++ app/src/main/java/com/example/kiliaro/data/db/sharedmedia/SharedMediaDao.kt
@@ -1,8 +1,18 @@
 package com.example.kiliaro.data.db.sharedmedia
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface SharedMediaDao {
+    @Query("SELECT * FROM $SHARED_MEDIA_TABLE_NAME WHERE $SHARED_MEDIA_COLUMN_SHARED_ID = :sharedId")
+    suspend fun getAll(sharedId: String): List<SharedMediaEntity>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(list: List<SharedMediaEntity>)
+
+    @Query("DELETE FROM $SHARED_MEDIA_TABLE_NAME WHERE $SHARED_MEDIA_COLUMN_SHARED_ID = :sharedId")
+    suspend fun deleteAll(sharedId:String)
 }
